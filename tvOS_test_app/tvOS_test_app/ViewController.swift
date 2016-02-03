22197afbefd47ff7c864afc2b45d96bf1417d1b2
Dan Bender <hello@danbender.net>
2016-02-03 21:33:11 +0100

    tvOS app prints hello world when it is built

---
+++ tvOS_test_app/tvOS_test_app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tvOS_test_app
+//
+//  Created by Dan Bender on 03/02/16.
+//  Copyright © 2016 Dan Bender. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
